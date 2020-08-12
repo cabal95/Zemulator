1aeceb7e60579f811d0059964522d20bc43f35e1
Daniel Hazelbaker <daniel@blueboxmoon.com>
2020-08-12 10:23:41 -0700

    Fixed some lingering trademarked references.

--- Zemulator.WPF/App.xaml.cs
+++ Zemulator.WPF/App.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace Zebra_Emulator
+namespace Zemulator.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
